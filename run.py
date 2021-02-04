79066205711e516193e05a1a23a0a98e17383903
Richardaeld <richardaeld@gmail.com>
2021-02-04 01:32:40 +0000

    Adapted application to run/display 'index' using flask

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -7,9 +11,17 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "here we go again"
+def index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
