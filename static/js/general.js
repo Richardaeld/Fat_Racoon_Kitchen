cd0d7b75c9077af97aa4c62b311e97d4d38804c9
Richardaeld <richardaeld@gmail.com>
2021-02-10 20:52:27 +0000

    Cleaned up code and increased setTimeout to correct time on 'general.js'

--- static/js/general.js
+++ static/js/general.js
@@ -1,11 +1,10 @@
-// Removes flash after 7 seconds
+// Removes all flash messages after 7 seconds
 if (document.getElementById("flash").getElementsByTagName("div")[0]){
-
     setTimeout( function () {
         flashRm = document.querySelectorAll(".flash").length
         for (i=0; i<flashRm; i++){
             var divRemove = document.getElementsByClassName("flash")[0];
             divRemove.remove()
         }
-    },3000)
+    },7000)
 }
