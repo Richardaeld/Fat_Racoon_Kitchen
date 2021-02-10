b30d8dab9f7328ccd396451fc920c71a9ac992f8
Richardaeld <richardaeld@gmail.com>
2021-02-10 20:49:13 +0000

    Improved flash(message) remove logic on 'general.js'

--- static/js/general.js
+++ static/js/general.js
@@ -1,6 +1,11 @@
 // Removes flash after 7 seconds
 if (document.getElementById("flash").getElementsByTagName("div")[0]){
+
     setTimeout( function () {
-        document.getElementById("flash").getElementsByTagName("div")[0].remove();
-    },7000)
+        flashRm = document.querySelectorAll(".flash").length
+        for (i=0; i<flashRm; i++){
+            var divRemove = document.getElementsByClassName("flash")[0];
+            divRemove.remove()
+        }
+    },3000)
 }
