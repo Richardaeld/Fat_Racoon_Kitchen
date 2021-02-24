e22bb27904d9d04ba7fdd3633cb52728cb8ae397
Richardaeld <richardaeld@gmail.com>
2021-02-24 22:17:22 +0000

    refactored location values and added commits 'loginCreateVal.js'

--- static/js/loginCreateVal.js
+++ static/js/loginCreateVal.js
@@ -1,3 +1,4 @@
+// ---- Login modal tab selector (login or create)
 // Makes the tab for login and create account clickable and switch between login and create account areas
 modaltabs = document.querySelectorAll(".tab");
 modaltabs.forEach(tabSelect);
@@ -5,21 +6,22 @@ function tabSelect(item, index) {
     item.addEventListener("click", function () {
         // Finds total to iterate over
         var totalCreate = document.querySelectorAll(".login-account").length;
-
+        var tabLoc = document.getElementsByClassName("create-account"); // user input div
         if(item.textContent.trim() === "Login"){
             // Makes all create account material invisible and removes required when login is clicked  
             for (i=0; i <totalCreate; i++){
-                document.getElementsByClassName("create-account")[i].classList.add("make-invis");
-                document.getElementsByClassName("create-account")[i].getElementsByTagName("input")[0].removeAttribute("required");
-                document.getElementsByClassName("create-account")[i].getElementsByTagName("input")[0].setAttribute("value", "");
-                document.getElementsByClassName("create-account")[i].getElementsByTagName("input")[0].value = "";
+                let tabInputLoc = document.getElementsByClassName("create-account")[i].getElementsByTagName("input")[0]; // user input value loc
+                tabLoc[i].classList.add("make-invis");
+                tabInputLoc.removeAttribute("required");
+                tabInputLoc.setAttribute("value", "");
+                tabInputLoc.value = "";
 
             }
         } else {
             // Makes all create account material visivle and makes adds required when create account is clicked
             for (i=0; i <totalCreate; i++){
-                document.getElementsByClassName("create-account")[i].classList.remove("make-invis");
-                document.getElementsByClassName("create-account")[i].getElementsByTagName("input")[0].setAttribute("required", "");
+                tabLoc[i].classList.remove("make-invis");
+                tabLoc[i].getElementsByTagName("input")[0].setAttribute("required", "");
             }
         }
 
