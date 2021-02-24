dd3c89f2555601fd0c73b1282c7c7c32a4535246
Richardaeld <richardaeld@gmail.com>
2021-02-24 19:22:57 +0000

    refactored JS for 'recipe.js'

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,9 +1,11 @@
+// ---- Cross out ingredients and steps
 // Finds all list items under class .recipe-lists and assigns text decoration line-through or none
 AllLists = document.querySelectorAll(".recipe-lists > li");
 AllLists.forEach(list);
 function list (item, index){
+    // Event cycles through text decoration for crossout effect
     item.addEventListener("click", function() {
-        var itemLoc =  item.getElementsByTagName("h6")[0]
+        let itemLoc =  item.getElementsByTagName("h6")[0]
         if (itemLoc.style.textDecoration == "line-through"){
             itemLoc.style.textDecoration = "none";
         } else {
@@ -11,47 +13,43 @@ function list (item, index){
         }
     })
 }
+// ---- Set Avatar Image size
+// if Avatar exists give it a height and width
+if (document.getElementById("avatarImg").getElementsByTagName("img")[0]){
+    // find height and width of container
+    let loc = document.getElementById("avatarImg");
+    let avatarHeight = loc.clientHeight;
+    let avatarWidth =  loc.clientWidth;
 
-// find height and width of container
-var crabHeight = document.getElementById("avatarImg").clientHeight;
-var crabWidth =  document.getElementById("avatarImg").clientWidth;
-console.log(document.getElementById("avatarImg").getBoundingClientRect())
+    // Alter height and width to compensate for padding
+    avatarWidth = (avatarWidth * 0.9) + "px";
+    avatarHeight = (avatarHeight * 0.9) + "px";
 
-// Alter height and width to compensate for padding
-crabWidth = (crabWidth * 0.9) + "px";
-crabHeight = (crabHeight * 0.9) + "px";
-console.log(crabHeight);
-console.log(crabWidth);
+    // Applying new height and width so image stays in container
+    loc.getElementsByTagName("img")[0].style.height = avatarHeight;
+    loc.getElementsByTagName("img")[0].style.width = avatarWidth;
+}
 
-// Applying new height and width so image stays in container
-//document.getElementById("avatarImg").getElementsByTagName("img")[0].style.height = crabHeight;
-//document.getElementById("avatarImg").getElementsByTagName("img")[0].style.width = crabWidth;
-
+// ---- Delete Modal
 // Open Delete Modal
 document.getElementById("deleteModal").addEventListener("click", function() {
     document.getElementsByClassName("custom-modal")[0].classList.remove("make-invis");
 })
-
+// Close Delete Modal
 document.getElementsByClassName("modal-background")[0].addEventListener("click", function () {
     document.getElementsByClassName("custom-modal")[0].classList.add("make-invis");
 })
 
 // Delete form validation logic
 document.getElementById("recipeName").addEventListener("keyup", function() {
+    let modalButton = document.getElementById("custom-button");
     let check = document.getElementById("checkName").getElementsByTagName("span")[0].getAttribute("value");
     let userinput = document.getElementById("recipeName").value;
-    console.log(userinput)
+
     // Apply disabled or ignore as needed
     if (check != userinput) {
-        if (document.getElementById("custom-button").hasAttribute("disbaled")){
-            //no nothing
-        } else {
-            document.getElementById("custom-button").setAttribute("disabled", "");
+        if (check == userinput){
+            modalButton.removeAttribute("disabled")
         }
-        console.log("nope")
-    // remove disabled if name and user input match
-    } else if (check == userinput){
-        console.log("correct")
-        document.getElementById("custom-button").removeAttribute("disabled")
     }
 })
