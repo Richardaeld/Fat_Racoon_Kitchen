f5eee24beac53b6e7a41e7147e57087bd2f7b701
Richardaeld <richardaeld@gmail.com>
2021-02-24 19:28:05 +0000

    Created user edit profile page

---
+++ static/js/edit_user_info.js
@@ -0,0 +1,30 @@
+// ---- Create Random Avatar name
+// Creates a random string for image name assignment
+const characterLibrary = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_@!&,?0123456789"
+const characterLibraryLength = characterLibrary.length;
+var imageName = ""
+for (let i=0; i<20; i++){
+    let characterLibraryIndex = Math.floor(Math.random()*(characterLibraryLength));
+    imageName += characterLibrary[characterLibraryIndex];
+}
+
+// ---- Apply Random Avatar Name to Avatar Image
+// Adds randomly generated image name
+document.getElementById("custom-button").addEventListener("click", function() {
+    document.getElementById("avatar_name").setAttribute("value", imageName);
+})
+
+// ---- Avatar Image Validation
+// Check file size
+document.getElementById("avatar").addEventListener("change", function() {
+    let loc = document.getElementById("avatar")
+    let fileSize = loc.files[0].size;
+    if (fileSize > 100000) {
+        loc.value = null;
+        loc.classList.add("invalid");
+        document.getElementById("avatar_valid").classList.remove("make-invis");
+    } else if (fileSize < 100000 && loc.classList.contains("invalid")){
+        loc.classList.remove("invalid");
+        document.getElementById("avatar_valid").classList.add("make-invis");
+    }
+})
