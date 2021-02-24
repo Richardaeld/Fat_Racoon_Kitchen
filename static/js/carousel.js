022c8a63858f1b497e4076e93449e9175c043ff4
Richardaeld <richardaeld@gmail.com>
2021-02-24 21:46:46 +0000

    Refactored and added better commits 'carousel.js'

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,16 +1,14 @@
+// ---- Carousel 
 // builds an array of carousel data
 var carousel = document.querySelectorAll(".card-carousel");
 
 // Find width of screen and sets amount of carousel boxes
 // This function is called by a global event handler and 
 // resizes according to the current width of screen
-var reveal;
+var reveal; // Amount of cards to reveal per responsiveness
 function findWidth(){
     let screenWidth;
-   // var reveal; 
-    screenWidth = window.screen.width;
-
-
+    screenWidth = window.screen.clientWidth; // Find screen width
 
     // Sets carousel number to be shown
     if (screenWidth > 1200) {
@@ -18,33 +16,25 @@ function findWidth(){
     } else if (screenWidth > 768) {
         reveal = 3;
     } else if (screenWidth > 576){
-        reveal = 2
+        reveal = 2;
     } else {
         reveal = 1;
     }
 
-    
-
-    // Finds the amount of visible carousel boxes
-    let visible = document.querySelectorAll(".card-carousel-vis")
+    let visible = document.querySelectorAll(".card-carousel-vis"); // Finds the amount of visible carousel boxes
 
     // Checks to see if boxes need to be removed
     if (visible.length >= reveal) {
-
         // Removes all carousel boxes
         for (let i=0; i <= visible.length-1; i++) {
             document.querySelector(".card-carousel-vis").classList.remove("card-carousel-vis");
         }
     }
-    
-    // Adds in carousel boxes
+    // Adds in correct amount of carousel boxes
     for (i=0; i<reveal; i++) {
-        carousel[i].classList.add("card-carousel-vis")
+        carousel[i].classList.add("card-carousel-vis");
     }
-
 }
-
-
 
 // initial setting of carousel boxes
 findWidth();
@@ -59,54 +49,50 @@ for(i=0; i<(carousel.length); i++){
     carouselArray.push(i);
 }
 
-// Left button on card carousel -- switches to previous carousel item
-document.getElementsByClassName("carousel-left")[0].addEventListener("click", function () {
-    console.log (reveal)
-    for (let i=0; i < reveal; i++){
-        document.getElementsByClassName("card-carousel")[carouselArray[i]].classList.remove("card-carousel-vis")
-    }
-    for (let i=0; i < reveal; i++){
-        let x = carouselArray.pop()
-        carouselArray.unshift(x)
-    }
-    for (let i=0; i < reveal; i++){
-        document.getElementsByClassName("card-carousel")[carouselArray[i]].classList.add("card-carousel-vis")
-    }
-})
+// Carousel button function
+var locCarouselArray =  document.getElementsByClassName("card-carousel")[carouselArray[i]]
+function carouselButton (button) {
+    document.getElementsByClassName(button)[0].addEventListener("click", function () {
+        for (let i=0; i < reveal; i++){
+            locCarouselArray.classList.remove("card-carousel-vis");
+        }
+        if (button == "carousel-right")
+            for (let i=0; i < reveal; i++){
+                let x = carouselArray.shift();
+                carouselArray.push(x);
+            }
+        else if (button == "carousel-left")
+            for (let i=0; i < reveal; i++){
+                let x = carouselArray.pop();
+                carouselArray.unshift(x);
+            }
+        for (let i=0; i < reveal; i++){
+            locCarouselArray.classList.add("card-carousel-vis");
+        }
+    })
+}
 
-// Right button on card carousel -- switches to next carousel item
-document.getElementsByClassName("carousel-right")[0].addEventListener("click", function () {
-    for (let i=0; i < reveal; i++){
-        document.getElementsByClassName("card-carousel")[carouselArray[i]].classList.remove("card-carousel-vis")
-    }
-    for (let i=0; i < reveal; i++){
-        let x = carouselArray.shift()
-        carouselArray.push(x)
-    }
-    for (i=0; i < reveal; i++){
-        document.getElementsByClassName("card-carousel")[carouselArray[i]].classList.add("card-carousel-vis")
-    }
-})
+carouselButton("carousel-right");
+carouselButton("carousel-left");
 
-
-
-
-
+// ---- login modal
 // Allows login modal to function and pop up
-var signInModal = document.querySelectorAll(".login-modal");
+let modal = document.getElementsByClassName("custom-modal")[0]; // modal
+let signInModal = document.querySelectorAll(".login-modal"); // login button
+let button = document.getElementsByClassName("custom-button")[0]; // submit button
 signInModal.forEach(popUpModal);
 function popUpModal(item, index){
     item.addEventListener("click", function () {
-        document.getElementsByClassName("custom-modal")[0].classList.remove("make-invis");
+        modal.classList.remove("make-invis");
     });
 }
 // makes modal disappear if background clicked
 document.getElementsByClassName("modal-background")[0].addEventListener("click", function() {
-    document.getElementsByClassName("custom-modal")[0].classList.add("make-invis");
+    modal.classList.add("make-invis");
 })
 
 // adds a different background to button when clicked
-document.getElementsByClassName("custom-button")[0].addEventListener("click", function () {
-    document.getElementsByClassName("custom-button")[0].classList.add("custom-button-press");
+button.addEventListener("click", function () {
+    button.classList.add("custom-button-press");
 })
 
