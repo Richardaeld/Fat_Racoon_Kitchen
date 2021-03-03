724be8a85f50131c39ce1b5a49131930f1883950
Richardaeld <richardaeld@gmail.com>
2021-03-03 07:32:24 +0000

    Updated lessons page to new style, massive UX improvement

---
+++ static/js/indexRotate.js
@@ -0,0 +1,13 @@
+var findIndexCards = document.querySelectorAll(".rotating-card");
+findIndexCards.forEach(flipIndex);
+function flipIndex(item, index) {
+    item.addEventListener("click", function () {
+        console.log(item.getElementsByClassName("index-rotate")[0].classList.contains("flip-index-card"))
+        if(item.getElementsByClassName("index-rotate")[0].classList.contains("flip-index-card") == true){
+            console.log("I tripped")
+            item.getElementsByClassName("index-rotate")[0].classList.remove("flip-index-card")
+        }else{
+            item.getElementsByClassName("index-rotate")[0].classList.add("flip-index-card")
+        }
+    })
+} 
