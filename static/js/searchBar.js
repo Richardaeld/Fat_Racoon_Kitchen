3ab2f8849da964158ff541e6b4fd3b9f7daaa35e
Richardaeld <richardaeld@gmail.com>
2021-03-07 02:16:56 +0000

    Added a message if search bar returns no results

--- static/js/searchBar.js
+++ static/js/searchBar.js
@@ -1,12 +1,14 @@
+// Makes search bar visible wen user clicks on it and adds defensive code
 var findSearchBar = document.querySelectorAll(".searchBar");
 findSearchBar.forEach(selectSearchBar);
 function selectSearchBar(item, index){
+    // Makes search bar visible with user click
     item.addEventListener("click", function () {
         item.getElementsByTagName("input")[0].classList.remove("make-invis")
 
     })
+    // Makes sure users are unable to submit blank queries
     item.getElementsByTagName("input")[0].addEventListener("keyup", function () {
-        console.log(";lksadjfklasj")
         if(item.getElementsByTagName("input")[0].value != "") {
             item.getElementsByTagName("button")[0].removeAttribute("disabled")
         }else if(item.getElementsByTagName("input")[0].value == "") {
@@ -15,3 +17,4 @@ function selectSearchBar(item, index){
     })
 
 }
+
