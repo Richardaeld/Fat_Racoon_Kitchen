a85dfd4a53bc7e647241d8dad4f3b22b2e091cdc
Richardaeld <richardaeld@gmail.com>
2021-03-07 01:27:29 +0000

    Improved function of search bar by adding defensive code

--- static/js/searchBar.js
+++ static/js/searchBar.js
@@ -3,13 +3,15 @@ findSearchBar.forEach(selectSearchBar);
 function selectSearchBar(item, index){
     item.addEventListener("click", function () {
         item.getElementsByTagName("input")[0].classList.remove("make-invis")
-    //    item.getElementsByTagName("input")[1].classList.remove("make-invis")
 
-        //if (item.getElementsByTagName("input")[0] == undefined){
-        //    var createInput = document.createElement("input");
-        //    createInput.className = "search-bar"
-        //    item.appendChild(createInput)
-        //}
+    })
+    item.getElementsByTagName("input")[0].addEventListener("keyup", function () {
+        console.log(";lksadjfklasj")
+        if(item.getElementsByTagName("input")[0].value != "") {
+            item.getElementsByTagName("button")[0].removeAttribute("disabled")
+        }else if(item.getElementsByTagName("input")[0].value == "") {
+            item.getElementsByTagName("button")[0].setAttribute("disabled", "")
+        }
     })
 
 }
