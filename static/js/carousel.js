b73e4259bd3b246ee109021439ee74a590de78f4
Richardaeld <richardaeld@gmail.com>
2021-03-23 19:19:34 +0000

    Improved responsiveness for index carousel

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -12,9 +12,9 @@ function findWidth(){
     // Sets carousel number to be shown
     if (screenWidth > 1200) {
         reveal = 3;
-    } else if (screenWidth > 768) {
+    } else if (screenWidth > 992) {
         reveal = 2;
-    } else if (screenWidth > 576){
+    } else if (screenWidth > 768){
         reveal = 1;
     } else {
         reveal = 1;
