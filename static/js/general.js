3990f9b22d0f33ade0692629ef70f0c86f81efad
Richardaeld <richardaeld@gmail.com>
2021-04-01 05:04:51 +0000

    Added missing semicolons

--- static/js/general.js
+++ static/js/general.js
@@ -1,30 +1,30 @@
 // Removes all flash messages after 7 seconds
 if (document.getElementById("flash").getElementsByTagName("div")[0]){
     setTimeout( function () {
-        flashRm = document.querySelectorAll(".flash").length
+        flashRm = document.querySelectorAll(".flash").length;
         for (i=0; i<flashRm; i++){
             var divRemove = document.getElementsByClassName("flash")[0];
-            divRemove.remove()
+            divRemove.remove();
         }
     },7000)
 }
 
 // Function to make social links have rippling liquid effect with dipping ice
 function makeRipples (targetLink) {
-    targetLiquid = targetLink.getElementsByTagName("span")[0]
-    targetIce = targetLink.getElementsByTagName("i")[0]
-    targetIce.classList.add("dipping-ice")
-    targetLiquid.classList.add("create-ripple")
+    targetLiquid = targetLink.getElementsByTagName("span")[0];
+    targetIce = targetLink.getElementsByTagName("i")[0];
+    targetIce.classList.add("dipping-ice");
+    targetLiquid.classList.add("create-ripple");
     // Resets animation after 3 seconds
     setTimeout(function () {
-        targetIce.classList.remove("dipping-ice")
-        targetLiquid.classList.remove("create-ripple")
+        targetIce.classList.remove("dipping-ice");
+        targetLiquid.classList.remove("create-ripple");
     },3000)
 }
 
 //Adds ripple effect to mouse over event listeners for social links
 var findLinks = document.querySelectorAll(".social-position");
-findLinks.forEach(selectLinks)
+findLinks.forEach(selectLinks);
 function selectLinks (link) {
     // for mouse users
     link.addEventListener("mouseenter", function() {
