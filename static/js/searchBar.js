21ae6ffa8bf18976fe23748f0e8a9e773317a3da
Richardaeld <richardaeld@gmail.com>
2021-04-01 04:29:38 +0000

    Updated variable names

--- static/js/searchBar.js
+++ static/js/searchBar.js
@@ -1,20 +1,22 @@
 // Makes search bar visible wen user clicks on it and adds defensive code
 var findSearchBar = document.querySelectorAll(".searchBar");
 findSearchBar.forEach(selectSearchBar);
-function selectSearchBar(item, index){
+function selectSearchBar(searchBar){
 
     // Makes search bar visible with user click
-    item.addEventListener("click", function () {
-        item.getElementsByTagName("input")[0].classList.remove("make-invis")
-        item.getElementsByTagName("input")[0].focus()
+    searchInput = searchBar.getElementsByTagName("input")[0]
+    searchBar.addEventListener("click", function () {
+        searchInput.classList.remove("make-invis")
+        searchInput.focus()
     })
 
     // Makes sure users are unable to submit blank queries
-    item.getElementsByTagName("input")[0].addEventListener("keyup", function () {
-        if(item.getElementsByTagName("input")[0].value != "") {
-            item.getElementsByTagName("button")[0].removeAttribute("disabled")
-        }else if(item.getElementsByTagName("input")[0].value == "") {
-            item.getElementsByTagName("button")[0].setAttribute("disabled", "")
+    searchButton = searchBar.getElementsByTagName("button")[0]
+    searchInput.addEventListener("keyup", function () {
+        if(searchInput.value != "") {
+            searchButton.removeAttribute("disabled")
+        }else if(searchInput.value == "") {
+            searchButton.setAttribute("disabled", "")
         }
     })
 
