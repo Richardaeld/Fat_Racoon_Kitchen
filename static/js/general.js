e4bbec508615576777dbdf165ee53a247ec7cd86
Richardaeld <richardaeld@gmail.com>
2021-04-01 18:57:34 +0000

    combined general.js and searchBar.js, updated their commits, and updated page links to them

--- static/js/general.js
+++ static/js/general.js
@@ -1,4 +1,4 @@
-// Removes all flash messages after 7 seconds
+// ----Removes all flash messages after 7 seconds
 if (document.getElementById("flash").getElementsByTagName("div")[0]){
     setTimeout( function () {
         flashRm = document.querySelectorAll(".flash").length;
@@ -9,6 +9,7 @@ if (document.getElementById("flash").getElementsByTagName("div")[0]){
     },7000)
 }
 
+// ----Social link animation
 // Function to make social links have rippling liquid effect with dipping ice
 function makeRipples (targetLink) {
     targetLiquid = targetLink.getElementsByTagName("span")[0];
@@ -34,5 +35,30 @@ function selectLinks (link) {
     link.addEventListener("touchmove", function() {
         makeRipples (link);
     })
+}
+
+// ----Search bar
+// Makes search bar visible wen user clicks on it and adds defensive code
+var findSearchBar = document.querySelectorAll(".searchBar");
+findSearchBar.forEach(selectSearchBar);
+function selectSearchBar(searchBar){
+
+    // Makes search bar visible with user click
+    searchInput = searchBar.getElementsByTagName("input")[0]
+    searchBar.addEventListener("click", function () {
+        searchInput.classList.remove("make-invis");
+        searchInput.focus();
+    })
+
+    // Makes sure users are unable to submit blank queries
+    searchButton = searchBar.getElementsByTagName("button")[0]
+    searchInput.addEventListener("keyup", function () {
+        if(searchInput.value != "") {
+            searchButton.removeAttribute("disabled");
+        }else if(searchInput.value == "") {
+            searchButton.setAttribute("disabled", "");
+        }
+    })
+
 }
 
