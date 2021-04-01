c0640112371c6c9b60894ae905aa850e13a7a135
Richardaeld <richardaeld@gmail.com>
2021-04-01 20:48:17 +0000

    Updated commits, removed console.logs, added semicolons, refactored code, updated generic variable names

--- static/js/pagination.js
+++ static/js/pagination.js
@@ -1,123 +1,105 @@
-
-// Creates a functional Pagination
+// ---- Pagination
+// Creates Pagination's frame
 var findFeature = document.querySelectorAll(".recipe-lists");
 findFeature.forEach(selectFeature);
-function selectFeature(item, index){
-    //console.log(item.getElementsByTagName("li")[0].getElementsByTagName("h"));
-//    console.log(item.getElementsByTagName("h1")[0].textContent);  //  ---- finds section name
-//    console.log(item.getElementsByTagName("li").length)  // ----- finds item count in section
-    let liLength = item.getElementsByTagName("li").length
+function selectFeature(pagination){
+    let liLength = pagination.getElementsByTagName("li").length;
     // Reveals column items that dont need pagination
     if(liLength < 5 && liLength >= 1){
-        for(let i = 0; i < liLength; i++ ){
-            item.getElementsByTagName("li")[i].classList.remove("make-invis");
+        for(let nonPag = 0; nonPag < liLength; nonPag++ ){
+            pagination.getElementsByTagName("li")[nonPag].classList.remove("make-invis");
         }
-    // Reveals items that need pagination
+    // Reveals column items that need pagination
     }else if(liLength > 5) {
-        for(let i = 0; i < 5; i++ ){
-            item.getElementsByTagName("li")[i].classList.remove("make-invis");
+        for(let usePag = 0; usePag < 5; usePag++ ){
+            pagination.getElementsByTagName("li")[usePag].classList.remove("make-invis");
         }
-        // Creates frame for pagination
+
+        // Creates DOM frame for pagination
         let createUl = document.createElement("ul");
-        createUl.className = "row no-gutters justify-content-center list-group list-group-horizontal recipe-lists-no-padding pagination-ul"
-        for (let i = 0; i < liLength/5; i++){
-            // Makes start ellipses
-            if (i == 0){
+        createUl.className = "row no-gutters justify-content-center list-group list-group-horizontal recipe-lists-no-padding pagination-ul";
+        for (let pagNumFrame = 0; pagNumFrame < liLength/5; pagNumFrame++){
+            // Creates start ellipses
+            if (pagNumFrame == 0){
                 let createLi = document.createElement("li");
-                createLi.className = "col list-group-item pagination-content first-ellipses make-invis"
-                createLi.textContent = "..."
-                createUl.appendChild(createLi)
+                createLi.className = "col list-group-item pagination-content first-ellipses make-invis";
+                createLi.textContent = "...";
+                createUl.appendChild(createLi);
             }
 
-            // Makes content of pagination
+            // Creates number content of pagination
             let createLi = document.createElement("li");
-            createLi.className = "col list-group-item pagination-content pagination-number"
-            createLi.textContent = i + 1
-            createUl.appendChild(createLi)
+            createLi.className = "col list-group-item pagination-content pagination-number";
+            createLi.textContent = pagNumFrame + 1;
+            createUl.appendChild(createLi);
 
-            // Makes end ellipses
-            console.log(Math.floor(liLength/5))
-            if (i == Math.floor(liLength/5) ){
+            // Creates end ellipses
+            if (pagNumFrame == Math.floor(liLength/5) ){
                 let createLi = document.createElement("li");
-                createLi.className = "col list-group-item pagination-content last-ellipses make-invis"
-                createLi.textContent = "..."
-                createUl.appendChild(createLi)
+                createLi.className = "col list-group-item pagination-content last-ellipses make-invis";
+                createLi.textContent = "...";
+                createUl.appendChild(createLi);
             }
         }
-        // Adds UL to DOM with all appropiate LI
-        item.insertAdjacentElement('afterend', createUl)
 
-        // Makes Pagination functional
-        var findPagination = item.parentElement.querySelectorAll(".pagination-number")
+        // Inserts UL to DOM with all appropiate LI
+        pagination.insertAdjacentElement('afterend', createUl);
+
+        // Creates Pagination functionality
+        var findPagination = pagination.parentElement.querySelectorAll(".pagination-number");
         findPagination.forEach(selectPagination);
-        function selectPagination(itemP, indexP){
-            itemP.addEventListener("click", function () {
-                // Find pagination position
-                paginationPosition = 5 * (parseInt(itemP.textContent)- 1)
-                
-                // Makes all items invisible 
-                for(let i = 0; i < liLength; i++ ){
-                    item.getElementsByTagName("li")[i].classList.add("make-invis");
+        function selectPagination(paginationNumber){;
+            paginationNumber.addEventListener("click", function () {
+                // Find pagination number position
+                paginationPosition = 5 * (parseInt(paginationNumber.textContent)- 1);
+                let paginationLi = pagination.getElementsByTagName("li");
+                // Hides all items that are invisible
+                for(let allInvis = 0; allInvis < liLength; allInvis++ ){
+                    paginationLi[allInvis].classList.add("make-invis");
                 }
 
                 // Reveals only items needed
-                for(let i = 0; i < 5; i++ ){
-                    if(item.getElementsByTagName("li")[i + paginationPosition]){
-                        item.getElementsByTagName("li")[i + paginationPosition].classList.remove("make-invis");
+                for(let removeInvis = 0; removeInvis < 5; removeInvis++ ){
+                    if(paginationLi[removeInvis + paginationPosition]){
+                        paginationLi[removeInvis + paginationPosition].classList.remove("make-invis");
                     } 
                 }
 
-
-                //console.log(itemP.parentElement.getElementsByClassName("pagination-number").length)
-                var paginationTotal = itemP.parentElement.getElementsByClassName("pagination-number").length
-                //reveals all pagination if invisible and removed selected class
-                for(let i = 0; i < paginationTotal; i++){
-                    let pagLoc = itemP.parentElement.getElementsByClassName("pagination-number")[i]
-                    pagLoc.classList.remove("make-invis")
-                    pagLoc.classList.remove("pagination-selected")
+                var paginationTotal = paginationNumber.parentElement.getElementsByClassName("pagination-number").length;
+                let pagNumLoc = paginationNumber.parentElement.getElementsByClassName("pagination-number");
+                //reveals all pagination if invisible and removes selected pagination class
+                for(let revealPagNum = 0; revealPagNum < paginationTotal; revealPagNum++){
+                    pagNumLoc[revealPagNum].classList.remove("make-invis");
+                    pagNumLoc[revealPagNum].classList.remove("pagination-selected");
                 }
 
-                // Adds seleceted class to clicked pagination
-                itemP.classList.add("pagination-selected")
+                // Adds seleceted class to clicked pagination number
+                paginationNumber.classList.add("pagination-selected");
+                let firstEllipLoc = paginationNumber.parentElement.getElementsByClassName("first-ellipses")[0]
+                let lastEllipLoc = paginationNumber.parentElement.getElementsByClassName("last-ellipses")[0]
+                //hides unused pagination and reveals appropiate ellipses
+                for(let invisPagNum = 0; invisPagNum < paginationTotal; invisPagNum++){
 
-                //hides extra pagination
-                for(let i = 0; i < paginationTotal; i++){
-                    let pagLoc = itemP.parentElement.getElementsByClassName("pagination-number")[i]                    
-
-                    // Removes pagination 2 greater than clicked pagination
-                    if(parseInt(itemP.textContent) +1 < parseInt(pagLoc.textContent) ){
-                    //    console.log("math")
-                    //    console.log(parseInt(itemP.textContent) +1)
-                    //    console.log(parseInt(pagLoc.textContent))
-                        pagLoc.classList.add("make-invis")
-                        itemP.parentElement.getElementsByClassName("last-ellipses")[0].classList.remove("make-invis")
+                    // Removes pagination 2 greater than clicked pagination and adds ellipses
+                    if(parseInt(paginationNumber.textContent) +1 < parseInt(pagNumLoc[invisPagNum].textContent) ){
+                        pagNumLoc[invisPagNum].classList.add("make-invis");
+                        lastEllipLoc.classList.remove("make-invis");
                     }
                     
-                    // Removes pagination 2 lesser than clicked pagination
-                    if(parseInt(itemP.textContent) -1 > parseInt(pagLoc.textContent) ){
-                    //    console.log("math")
-                    //    console.log(parseInt(itemP.textContent) +1)
-                    //    console.log(parseInt(pagLoc.textContent))
-                        pagLoc.classList.add("make-invis")
-                        itemP.parentElement.getElementsByClassName("first-ellipses")[0].classList.remove("make-invis")
+                    // Removes pagination 2 lesser than clicked pagination and adds ellipses
+                    if(parseInt(paginationNumber.textContent) -1 > parseInt(pagNumLoc[invisPagNum].textContent) ){
+                        pagNumLoc[invisPagNum].classList.add("make-invis");
+                        firstEllipLoc.classList.remove("make-invis");
                     }
-
                 }
 
-                // Checks for ellipses to be removed
-                if(itemP.parentElement.getElementsByClassName("pagination-number")[0].classList.contains('make-invis') != true){
-                    itemP.parentElement.getElementsByClassName("first-ellipses")[0].classList.add("make-invis")
+                // Checks for start/end ellipses to be removed
+                if(pagNumLoc[0].classList.contains('make-invis') != true){
+                    firstEllipLoc.classList.add("make-invis");
+                } else if(pagNumLoc[parseInt(paginationTotal)-1].classList.contains('make-invis') != true){
+                    lastEllipLoc.classList.add("make-invis");
                 }
-
-                console.log(paginationTotal)
-                if(itemP.parentElement.getElementsByClassName("pagination-number")[parseInt(paginationTotal)-1].classList.contains('make-invis') != true){
-                    itemP.parentElement.getElementsByClassName("last-ellipses")[0].classList.add("make-invis")
-                }
-
             })
         }
-
     }
-
 }
-
