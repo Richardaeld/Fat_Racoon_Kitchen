e76cb8453af5c5c54e5c298dcaea301722e45988
Richardaeld <richardaeld@gmail.com>
2021-04-02 00:52:56 +0000

    Improved commits, added semicolons, named generic variables

--- static/js/loginCreateModal.js
+++ static/js/loginCreateModal.js
@@ -1,20 +1,20 @@
 // ---- login modal
-// Allows login modal to function and pop up
+// Allows login/create modal to open
 let modal = document.getElementsByClassName("custom-modal")[0]; // modal
 let signInModal = document.querySelectorAll(".login-modal"); // login button
 let button = document.getElementsByClassName("custom-button")[0]; // submit button
 signInModal.forEach(popUpModal);
-function popUpModal(item, index){
-    item.addEventListener("click", function () {
+function popUpModal(openModal){
+    openModal.addEventListener("click", function () {
         modal.classList.remove("make-invis");
     });
 }
-// makes modal disappear if background clicked
+// Removes login/create modal if background clicked
 document.getElementsByClassName("modal-background")[0].addEventListener("click", function() {
     modal.classList.add("make-invis");
 })
 
-// adds a different background to button when clicked
+// Creates a depressed look if button clicked
 button.addEventListener("click", function () {
     button.classList.add("custom-button-press");
 })
@@ -23,50 +23,49 @@ button.addEventListener("click", function () {
 // Makes the tab for login and create account clickable and switch between login and create account areas
 modaltabs = document.querySelectorAll(".tab");
 modaltabs.forEach(tabSelect);
-function tabSelect(item, index) {
-    item.addEventListener("click", function () {
-        // Finds total to iterate over
+function tabSelect(modalTab) {
+    modalTab.addEventListener("click", function () {
         var totalCreate = document.querySelectorAll(".login-account").length;
         var tabLoc = document.getElementsByClassName("create-account"); // user input div
-        if(item.textContent.trim() === "Login"){
-            // Makes all create account material invisible and removes required when login is clicked  
-            for (i=0; i <totalCreate-1; i++){
-                let tabInputLoc = document.getElementsByClassName("create-account")[i].getElementsByTagName("input")[0]; // user input value loc
-                tabLoc[i].classList.add("make-invis");
+        // Applies login settings
+        if(modalTab.textContent.trim() === "Login"){
+            // Removes all create account material form modal
+            for (removeCreate = 0; removeCreate < totalCreate - 1; removeCreate++){
+                let tabInputLoc = document.getElementsByClassName("create-account")[removeCreate].getElementsByTagName("input")[0]; // user input value loc
+                tabLoc[removeCreate].classList.add("make-invis");
                 tabInputLoc.removeAttribute("required");
                 tabInputLoc.setAttribute("value", "");
                 tabInputLoc.value = "";
-
             }
             //sets submit button value
-            document.getElementById("custom-button").value = "Login"
-            // Undoes auto fail for password check so it does not disrupt user login on login mode            
-            document.getElementById("passwordCheck2").parentElement.getElementsByTagName("p")[1].classList.add("make-invis")
+            document.getElementById("custom-button").value = "Login";
+            // Undoes auto fail for password check so it does not disrupt user login on login mode
+            document.getElementById("passwordCheck2").parentElement.getElementsByTagName("p")[1].classList.add("make-invis");
             // Makes sure username does not auto fail validation
-            document.getElementById("name").parentElement.getElementsByTagName("p")[0].classList.add("make-invis")
+            document.getElementById("name").parentElement.getElementsByTagName("p")[0].classList.add("make-invis");
             // Checks Validation
-            formIsValid = true
-            finalValidation()
+            formIsValid = true;
+            finalValidation();
 
-
+        // Applies create settings
         } else {
-            // Makes all create account material visivle and makes adds required when create account is clicked
-            for (i=0; i <totalCreate-1; i++){
-                tabLoc[i].classList.remove("make-invis");
-                tabLoc[i].getElementsByTagName("input")[0].setAttribute("required", "");
+            // Adds all create account material to modal
+            for (addCreate = 0; addCreate < totalCreate - 1; addCreate++){
+                tabLoc[addCreate].classList.remove("make-invis");
+                tabLoc[addCreate].getElementsByTagName("input")[0].setAttribute("required", "");
             }
-            // Sets submit button value
-            document.getElementById("custom-button").value = "Create"
+            // Sets submit button to create value
+            document.getElementById("custom-button").value = "Create";
             // Makes sure confirm password does not auto validate
-            document.getElementById("passwordCheck2").parentElement.getElementsByTagName("p")[1].classList.remove("make-invis")
+            document.getElementById("passwordCheck2").parentElement.getElementsByTagName("p")[1].classList.remove("make-invis");
             // Makes sure username does not auto validate
-            document.getElementById("name").parentElement.getElementsByTagName("p")[0].classList.remove("make-invis")
+            document.getElementById("name").parentElement.getElementsByTagName("p")[0].classList.remove("make-invis");
             // Checks Validation
-            finalValidation()
+            finalValidation();
         }
 
         // Changes the filter from Login to create account on click
         document.querySelector(".tab-selected").classList.remove("tab-selected");
-        item.classList.add("tab-selected")
+        modalTab.classList.add("tab-selected");
     });
 }
