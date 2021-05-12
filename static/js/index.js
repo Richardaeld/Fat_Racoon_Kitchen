26428dd900685373f181301410da904e914b44a4
Richardaeld <richardaeld@gmail.com>
2021-05-12 20:31:43 +0000

    Cleaned up code, imporved/added commits and added click effect to submit buttons

--- static/js/index.js
+++ static/js/index.js
@@ -1,41 +1,34 @@
-// ---- Password Validation ----
 // ---- Validation ----
-// ----Validates matching passwords -- passwordCheck1 and passwordCheck2
+// ----Validates matching passwords
+// Compares these id targets: passwordCheck1 and passwordCheck2 and adds/removes invalid commit
 let passwordCheck1 = document.getElementById("passwordCheck1"); // Compare location 1
 let passwordCheck2 = document.getElementById("passwordCheck2"); // Compare location 2
 var findPasswordComapre = document.querySelectorAll(".passwordCompare");
 findPasswordComapre.forEach(selectPasswordComapre);
 function selectPasswordComapre(comparePassLoc) {
     comparePassLoc.addEventListener("keyup", function () {
-        let passCheck1Val = passwordCheck1.parentElement.getElementsByTagName("p")[1]; // Password 
-        let passCheck2Val = passwordCheck2.parentElement.getElementsByTagName("p")[1]; // Password check
-        // Adds invalid bubble
+        let passCheck1Valid = passwordCheck1.parentElement.getElementsByTagName("p")[1];  // Location of passwordCheck1 match description
+        let passCheck2Valid = passwordCheck2.parentElement.getElementsByTagName("p")[1];  // Location of passwordCheck2 match description
+        // Adds/Removes password compare bubble
         if(passwordCheck1.value == passwordCheck2.value){
-            passCheck1Val.classList.add("make-invis");
-            passCheck2Val.classList.add("make-invis");
+            passCheck1Valid.classList.add("make-invis");
+            passCheck2Valid.classList.add("make-invis");
         }else{
-            passCheck1Val.classList.remove("make-invis");
-            passCheck2Val.classList.remove("make-invis");
+            passCheck1Valid.classList.remove("make-invis");
+            passCheck2Valid.classList.remove("make-invis");
         }
-        // Runs final validation if create account
-        if (comparePassLoc.classList.contains("createValidation")) {
-            finalValidation(".passwordCompare");
-        }
+        // Runs final validation for password compare
+        finalValidation(".passwordCompare");
     });
 }
 
-// ---- Form Validation for passwords
-//form REGEX
-// const matchAcceptedInput = /[]/;
+// Validation REGEX
 const matchTypeUpper = /[A-Z]/;
 const matchTypeLower = /[a-z]/;
 const matchTypeNumber = /[0-9]/;
-// const matchTypeChatacter = /[.|@|%]/;
-// const matchTypeBadCharacter = /['|"|$|,]/;
-
 const matchTypeGoodCharacter = /[!|@|#|%|&|*|_|+|=|?|.|'|/|-]/;
 const matchTypeTextCharacter = /[,|"]/;
-const matchTypeSpaces =  /[ ]/g; // Follow with g to make global
+const matchTypeSpaces =  /[ ]/g;  // Globally checks for spaces
 const matchTypeAtSign = /[@]/;
 const matchTypeDotCom = /.com/;
 const matchTypeDotEdu = /.edu/;
@@ -43,12 +36,12 @@ const matchTypeDotNet = /.net/;
 const matchTypeDotOrg = /.org/;
 
 var formIsValid = false;  // Presets validation for initial run
-// var invalidOnlyOne = false;  // Presets validation for initial run // not needed as initial
 
-// ----- Final Validation check
-// Sets or removes invalid bubble and invalid attributes
+// ----- Final Validation check 
+// Adds/Removes disabled from submit button according to invalids found on form
+// Adds/Removes parent validation container to be visible or invisible for invalid commits
+// Adds/Removes invalid color to input container
 function finalValidation(validationLoc) {
-
     let findFormInputs = document.querySelectorAll(validationLoc);
     findFormInputs.forEach(selectFormInputs);
     function selectFormInputs(finalVal) {
@@ -56,7 +49,7 @@ function finalValidation(validationLoc) {
         currentFormValid = true // Sets macro form validation setting
 
         let checkPLength = finalVal.parentElement.getElementsByTagName("p");  // Finds all p's to iteration validation over
-        //enables and disables validation bubble according to if input is valid or not
+        // Searches form for invalid input and sets Global/Macro validation
         for (let i=0; i< checkPLength.length; i++){
             let validCheck = finalVal.parentElement.getElementsByTagName("p")[i].classList.contains("make-invis")
             if(validCheck == false){
@@ -65,7 +58,7 @@ function finalValidation(validationLoc) {
             }
         }
 
-        // Validates on global form
+        // Adds/Removes disable to submit button according to validation (Global Validation)
         if(formIsValid) {
             document.getElementById("custom-button-login").removeAttribute("disabled");
             document.getElementById("custom-button-create").removeAttribute("disabled");
@@ -73,13 +66,14 @@ function finalValidation(validationLoc) {
             document.getElementById("custom-button-login").setAttribute("disabled", "");
             document.getElementById("custom-button-create").setAttribute("disabled", "");
         }
-        // Validates on macro level
+        // Adds/Removes invalid color
+        // Adds/Removes parent validation container's abaility to display (Macro Validation)
         if (currentFormValid) {
             finalVal.parentElement.getElementsByTagName("div")[0].classList.add("form-is-valid");
             finalVal.removeAttribute("invalid");
             finalVal.classList.remove("form-invalid-view");
         } else {
-            // Reveals box if form invalidates
+            // Reveals parent validation container of clicked item if form invalidates
             if (document.activeElement == finalVal){
                 finalVal.parentElement.getElementsByTagName("div")[0].classList.remove("make-invis")
             }
@@ -90,36 +84,33 @@ function finalValidation(validationLoc) {
     }
 }
 
-
-
 // ---- Base Validation starting point
 // basic (start) validation function
 function baseValidation (inputSelector, validationSelector, userInputType) {
     var findPasswords = document.querySelectorAll(inputSelector);
     findPasswords.forEach(selectPasswords);
     function selectPasswords(baseVal){
-        let baseValPara = baseVal.parentElement.getElementsByTagName("p");
+        let baseValPara = baseVal.parentElement.getElementsByTagName("p"); //  Finds the location of the invalid commits
 
         // Makes validation bubble visible on focus 
         baseVal.addEventListener("focus", function() {
-            validCheck = true
+            validCheck = true  // Presets valitation to valid
             let checkPLengthTemp = baseVal.parentElement.getElementsByTagName("p");  // Finds all p's to iteration validation over
-            
-            //enables and disables validation bubble according to if input is valid or not
+            // Enables validation bubble according to Macro Validation
             for (let i=0; i< checkPLengthTemp.length; i++){
-                // Sets a validation variable as it checks over all possible invalidation points
-                validCheck = baseVal.parentElement.getElementsByTagName("p")[i].classList.contains("make-invis")
+                validCheck = baseVal.parentElement.getElementsByTagName("p")[i].classList.contains("make-invis")  // Sets variable according to if its valid
+                // Sets Macro Validation variable
                 if(validCheck == false){
                     formIsValid = false;
                 }
             }
-
+            // Disables validation bubble according to Macro Validation
             if (formIsValid == false) {
                 baseVal.parentElement.getElementsByTagName("div")[0].classList.remove("make-invis");
             }
         });
 
-        // Makes validation bubble hidden on blur
+        // Hides validation bubble on blur
         baseVal.addEventListener("blur", function() {
             baseVal.parentElement.getElementsByTagName("div")[0].classList.add("make-invis");
         });
@@ -130,23 +121,11 @@ function baseValidation (inputSelector, validationSelector, userInputType) {
             if(baseVal.value.match(matchTypeSpaces)){
                 baseVal.value = baseVal.value.replace(matchTypeSpaces, "_");
             }
-            
-            // // Checks for improper characters and invalidates if found
-            // if(baseVal.value.match(matchTypeBadCharacter) != null){
-            //     baseValPara[2].classList.remove("make-invis");
-            // } else {
-            //     baseValPara[2].classList.add("make-invis");
-            // }
 
-// matchTypeUpper
-// matchTypeLower
-// matchTypeNumber
-
-            // Checks for improper characters and invalidates if found
-
+            // Iterates over all individual input characters
             for (i=0; i< baseVal.value.length; i++){
-                char = baseVal.value[i]
-                
+                char = baseVal.value[i]  // Character being checked
+                // Checks for improper characters and sets validation
                 if (char.match(matchTypeGoodCharacter) == null && char.match(matchTypeUpper) == null && char.match(matchTypeLower) == null && char.match(matchTypeNumber) == null) {
                     baseValPara[2].classList.remove("make-invis");
                     break
@@ -155,21 +134,9 @@ function baseValidation (inputSelector, validationSelector, userInputType) {
                 }
             }
 
-            // if(baseVal.value.match(matchTypeGoodCharacter) != null){
-            //     baseValPara[2].classList.remove("make-invis");
-            // } else {
-            //     baseValPara[2].classList.add("make-invis");
-            // }
-
-
-            
-
-
-
             // Validation for password
             if(validationSelector === "password"){
-
-                // Validates the form if correct amount of upper character, total characters and number are found
+                // Validates the form if correct amount of upper character, total characters, numbers and total characters are found
                 if(baseVal.value.match(matchTypeUpper) && baseVal.value.match(matchTypeLower) && baseVal.value.match(matchTypeNumber) && baseVal.value.length >= 8 && baseVal.value.length <= 20){
                     baseValPara[0].classList.add("make-invis");
                 }else{
@@ -178,8 +145,7 @@ function baseValidation (inputSelector, validationSelector, userInputType) {
 
             // Validation for email
             } else if(validationSelector === "email") {
-
-                // Validates for length
+                // Validates for total characters found
                 if (baseVal.value.length >= 4 && baseVal.value.length <= 255){
                     baseValPara[0].classList.add("make-invis");
                 } else {
@@ -189,7 +155,7 @@ function baseValidation (inputSelector, validationSelector, userInputType) {
                 // Validates for email suffix ('.com', '.edu', '.net') and '@'
                 let emailLength = baseVal.value.length;
                 if  (emailLength >= 4 ){
-                    var checkEmailValue = "";
+                    var checkEmailValue = "";  // Presets string to add suffix email to
                     // Uses last 4 digits to check for suffix
                     for (let i = 3; i >= 0; i--){
                         checkEmailValue += baseVal.value[(emailLength - 1) - i];
@@ -211,8 +177,7 @@ function baseValidation (inputSelector, validationSelector, userInputType) {
                     }
                 }
 
-                
-            //Validates for name/username
+            // Validates for recipe name/username
             } else if (validationSelector === "name"){
                 // Validates if correct amount of characters present
                 if(baseVal.value.length >= 4 && baseVal.value.length <= 100){
@@ -222,9 +187,8 @@ function baseValidation (inputSelector, validationSelector, userInputType) {
                 }
             }
 
-            // final validation of form
+            // final validation (Global/Macro) of form for login/create forms
             formIsValid = true;
-            // invalidOnlyOne = false;
             if(baseVal.classList.contains("loginValidation")){
                 finalValidation(".loginValidation");
             } else if (baseVal.classList.contains("createValidation")) {
@@ -247,7 +211,7 @@ baseValidation(".passwordSets", "password", "keyup"); // keyboard
 
 // ---- Login Create Modal ----
 // ---- login modal
-// Opens/closes modal's aside
+// Opens/close modal of login/create form
 let modal = document.getElementById("modalAside"); // modal
 let signInModal = document.querySelectorAll(".login-modal"); // login button
 signInModal.forEach(popUpModal);
@@ -257,21 +221,21 @@ function popUpModal(openModal){
         document.getElementById("email").focus()
     });
 }
+// Closes modal if background filter is clicked
 document.getElementsByClassName("modal-background")[0].addEventListener("click", function() {
     modal.classList.add("make-invis");
 });
 
-// Creates a depressed look if button clicked
-let button = document.getElementsByClassName("custom-button")[0]; // submit button
+// Creates a depressed look if submit button clicked
 document.getElementById("custom-button-create").addEventListener("click", function () {
-    button.classList.add("custom-button-press");
+    document.getElementById("custom-button-create").classList.add("custom-button-press");
 });
 document.getElementById("custom-button-login").addEventListener("click", function () {
-    button.classList.add("custom-button-press");
+    document.getElementById("custom-button-login").classList.add("custom-button-press");
 });
 
-// ---- Login modal tab selector (login or create)
-// Makes the tab for login and create account clickable and switch between login and create account areas
+// ---- Login/Create modal tab selector (login or create)
+// Makes the tabs for login/create clickable and switches between login and create account forms
 var modaltabs = document.querySelectorAll(".tab");
 modaltabs.forEach(tabSelect);
 function tabSelect(modalTab) {
@@ -285,8 +249,7 @@ function tabSelect(modalTab) {
                 userInputLoc.parentElement.getElementsByTagName("p")[0].classList.remove("make-invis");
             }
         }
-
-        var totalCreate = document.querySelectorAll(".formValidation").length;
+        var totalCreate = document.querySelectorAll(".formValidation").length;  // sets length of inputs to be erased 
         // Opens login/create modal and resets user inputs
         if(modalTab.textContent.trim() === "Login"){
             document.getElementById("login-modal").classList.remove("make-invis");
@@ -304,9 +267,8 @@ function tabSelect(modalTab) {
     });
 }
 
-
 // ---- General ----
-// ----Removes all flash messages after 7 seconds
+// ----Removes all flash messages after 10 seconds
 if (document.getElementById("flash").getElementsByTagName("div")[0]){
     setTimeout( function () {
         let flashRm = document.querySelectorAll(".flash").length;
@@ -347,7 +309,7 @@ function selectLinks (link) {
 }
 
 
-// ----button ----
+// ---- Button (Non-form submit buttons) ----
 let jsButtonFind = document.querySelectorAll(".custom-js-button");
 jsButtonFind.forEach(jsButtonSelect);
 function jsButtonSelect(button) {
@@ -362,7 +324,6 @@ function jsButtonSelect(button) {
         }
     });
 }
-
 
 // ---- Carousel ----
 // builds an array of carousel data
@@ -383,7 +344,7 @@ function findWidth(){
 
     let screenWidth;
     screenWidth = document.body.offsetWidth; // Find screen width
-    // Sets carousel number to be shown
+    // Sets carousel card amount to be shown
     if (screenWidth > 1200) {
         reveal = 3;
     } else if (screenWidth > 992) {
@@ -456,4 +417,3 @@ function carouselButton (button) {
 carouselButton("carousel-right");
 // Calls carousel to move left
 carouselButton("carousel-left");
-
