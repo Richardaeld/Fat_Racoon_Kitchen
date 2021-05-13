489c9dc3844ff42bdad74e079e21f4ce2fb699a0
Richardaeld <richardaeld@gmail.com>
2021-05-13 03:06:56 +0000

    Removed unused variables from button logic

--- static/js/editUserInfo.js
+++ static/js/editUserInfo.js
@@ -329,11 +329,11 @@ document.getElementById("avatar").addEventListener("change", function() {
 let jsButtonFind = document.querySelectorAll(".custom-js-button");
 jsButtonFind.forEach(jsButtonSelect);
 function jsButtonSelect(button) {
-    button.addEventListener("click", function(pushButton){
+    button.addEventListener("click", function(){
         button.classList.add("custom-button-press-outer");
         button.getElementsByClassName("sticky-note-inner-div")[0].classList.add("custom-button-press");
         if(button.id == "deleteModal"){
-            setTimeout(function(unpushButton){
+            setTimeout(function(){
                 button.classList.remove("custom-button-press-outer");
                 button.getElementsByClassName("sticky-note-inner-div")[0].classList.remove("custom-button-press");
             },3000);
--- static/js/profile.js
+++ static/js/profile.js
@@ -44,11 +44,11 @@ function selectLinks (link) {
 let jsButtonFind = document.querySelectorAll(".custom-js-button");
 jsButtonFind.forEach(jsButtonSelect);
 function jsButtonSelect(button) {
-    button.addEventListener("click", function(pushButton){
+    button.addEventListener("click", function(){
         button.classList.add("custom-button-press-outer");
         button.getElementsByClassName("sticky-note-inner-div")[0].classList.add("custom-button-press");
         if(button.id == "deleteModal"){
-            setTimeout(function(unpushButton){
+            setTimeout(function(){
                 button.classList.remove("custom-button-press-outer");
                 button.getElementsByClassName("sticky-note-inner-div")[0].classList.remove("custom-button-press");
             },3000);
