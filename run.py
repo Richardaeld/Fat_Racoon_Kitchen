178d30e567644926a449148ba13de596886198aa
Richardaeld <richardaeld@gmail.com>
2021-06-09 22:07:11 +0000

    Updated debug status to False

--- run.py
+++ run.py
@@ -342,4 +342,4 @@ def search_user_uploads():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
